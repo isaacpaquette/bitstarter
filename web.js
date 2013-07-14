eed14389982f3b23c77e16dc61f06e23ce1fecdd
isaac.paquette <isaac.paquette@gmail.com>
2013-07-14 09:00:01 -0400

    consume index.html in web.js - use buffer

--- web.js
+++ web.js
@@ -6,7 +6,8 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
   	fs.readFileSync('index.html', function (err, data) {
 	  if (err) throw err;
-	  response.send(data.toString('utf-8'));
+	  var buffer = new Buffer(data, "utf-8")
+	  response.send(buffer.toString('utf-8'));
 	});	
 });
 
